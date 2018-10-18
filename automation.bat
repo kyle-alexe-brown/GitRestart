25c17978af11351510c9b5e72cb4e7b8554042d5
kyle-alexe-brown <kyle.brown@cogworks.co.za>
2018-10-18 09:33:01 +0200

    auto push

---
+++ automation.bat
@@ -0,0 +1,18 @@
+:loop
+	
+	cd <path> C:\Users\kyleb\Desktop\Gitfrmover
+	
+	git init
+	
+	
+	git pull
+	
+	git add --all
+	
+	git commit -m "auto push"
+	
+	git push
+	
+	TIMEOUT 300
+
+goto loop
---
+++ login.bat
@@ -0,0 +1,4 @@
+$ git config credential.helper store
+$ git push https://github.com/<kyle-alexe-brown>/<GitRestart>.git
+Username for 'https://github.com': <kyle-alexe-brown>
+Password for 'https://USERNAME@github.com': <Skaapkop123>
